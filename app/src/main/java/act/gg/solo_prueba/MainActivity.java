3dc84c0861b3a039801fec6c01b2829c0a646745
Nico-moraga1 <Nicolasmoraga27@gmail.com>
2020-11-18 00:03:25 -0300

    Initial commit

---
+++ app/src/main/java/act/gg/solo_prueba/MainActivity.java
@@ -0,0 +1,14 @@
+package act.gg.solo_prueba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
